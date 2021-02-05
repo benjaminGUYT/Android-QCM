e51e848915446d8ed723f9e6d1835ab8ebbb1358
benjaminGUYT <55363637+benjaminGUYT@users.noreply.github.com>
2021-02-05 22:57:37 +0100

    Nettoyage pre mise en place ModelView

--- app/src/main/java/com/example/qcm/ui/qcm/QcmFragment.java
+++ app/src/main/java/com/example/qcm/ui/qcm/QcmFragment.java
@@ -17,7 +17,6 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.Filter;
 import android.widget.RadioButton;
-import android.widget.Toast;
 
 import com.example.qcm.R;
 import com.example.qcm.models.ListQuestions;
@@ -39,17 +38,19 @@ public class QcmFragment extends Fragment {
 
     private List<UserResponse> userResponses;
 
+    public QcmFragment() {
+        // Required empty public constructor
+    }
+
     public void setListQuestions(ListQuestions listQuestions) {
         this.listQuestions = listQuestions;
     }
 
     private ListQuestions listQuestions;
-    Question question;
-
-    public QcmFragment() {
-        // Required empty public constructor
-    }
-
+    private Question question;
+    private Button next;
+    private MultipleQuestionWidget mqw;
+    private TrueFalseQuestionWidget tfqw;
 
     public static QcmFragment newInstance(ListQuestions listQuestions) {
         QcmFragment fragment = new QcmFragment();
@@ -62,51 +63,74 @@ public class QcmFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
-    @SuppressLint("ResourceType")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         View root = inflater.inflate(R.layout.fragment_qcm, container, false);
 
         userResponses = new ArrayList<>();
-        MultipleQuestionWidget mqw = root.findViewById(R.id.question);
-        TrueFalseQuestionWidget tfqw = root.findViewById(R.id.trueFalse);
+        mqw = root.findViewById(R.id.question);
+        tfqw = root.findViewById(R.id.trueFalse);
+        next = root.findViewById(R.id.buttonNext);
 
-        Button next = root.findViewById(R.id.buttonNext);
+        setNextQuestion();
 
+        return root;
+    }
+
+    private void setNextQuestion() {
         question = listQuestions.getResults().remove(0);
-        if(listQuestions.getResults().size() == 0) {
+        if(listQuestions.getResults().isEmpty()) setNextButtonFinalState();
+        else setNextButton();
+        setWidgetsVisibility();
+    }
 
-            next.setText("Terminer");
-            next.setOnClickListener(view1 -> {
-                if(mqw.getVisibility() == View.VISIBLE) {
-                    userResponses.add(mqw.getUserResponses());
-                    List<CheckBox> responseCheckBox = mqw.getReponsesCheckBox();
-                    for(CheckBox c : responseCheckBox) {
-                        if(c.getText().toString().equals(question.getCorrect_answer()))
-                            c.setTextColor(Color.GREEN);
-                        else c.setTextColor(Color.RED);
-                    }
-                }
-                else if(tfqw.getVisibility() == View.VISIBLE) {
-                    userResponses.add(tfqw.getUserResponses());
-                    List<RadioButton> responseCheckBox = tfqw.getReponsesRadioButton();
-                    for(RadioButton c : responseCheckBox) {
-                        if(c.getText().toString().equals(question.getCorrect_answer()))
-                            c.setTextColor(Color.GREEN);
-                        else c.setTextColor(Color.RED);
-                    }
-                }
-                FragmentTransaction t = getParentFragmentManager().beginTransaction();
-                t.replace(R.id.nav_host_fragment, EndFragment.newInstance(userResponses));
-                t.commit();
+    private void setNextButton() {
+        next.setOnClickListener(view -> {
+            showSolution();
+            Handler handler = new Handler();
+            handler.postDelayed(() -> {
+                if (mqw.getVisibility() == View.VISIBLE)
+                    mqw.reset();
+                if (tfqw.getVisibility() == View.VISIBLE)
+                    tfqw.reset();
+                handler.postDelayed(() -> setNextQuestion(), 200);
+            }, 1800);
+        });
+    }
 
+    private void setNextButtonFinalState() {
+        next.setText("Terminer");
+        next.setOnClickListener(view1 -> {
+            showSolution();
+            FragmentTransaction t = getParentFragmentManager().beginTransaction();
+            t.replace(R.id.nav_host_fragment, EndFragment.newInstance(userResponses));
+            t.commit();
+        });
+    }
 
+    private void showSolution() {
+        if (mqw.getVisibility() == View.VISIBLE) {
+            userResponses.add(mqw.getUserResponses());
+            List<CheckBox> responseCheckBox = mqw.getReponsesCheckBox();
+            for (CheckBox c : responseCheckBox) {
+                if (c.getText().toString().equals(question.getCorrect_answer()))
+                    c.setTextColor(Color.GREEN);
+                else c.setTextColor(Color.RED);
             }
-            );
+        } else if (tfqw.getVisibility() == View.VISIBLE) {
+            userResponses.add(tfqw.getUserResponses());
+            List<RadioButton> responseCheckBox = tfqw.getReponsesRadioButton();
+            for (RadioButton c : responseCheckBox) {
+                if (c.getText().toString().equals(question.getCorrect_answer()))
+                    c.setTextColor(Color.GREEN);
+                else c.setTextColor(Color.RED);
+            }
         }
+    }
 
+    private void setWidgetsVisibility() {
         if(question.getType().equals("multiple")) {
             mqw.setVisibility(View.VISIBLE);
             tfqw.setVisibility(View.INVISIBLE);
@@ -117,127 +141,6 @@ public class QcmFragment extends Fragment {
             mqw.setVisibility(View.INVISIBLE);
             tfqw.setQuestion(question);
         }
-        if(listQuestions.getResults().size() != 0) {
-            next.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-
-                    System.out.println(mqw.isAllCheckBoxUnchecked());
-                    System.out.println(tfqw.isAllRadioButtonUnchecked());
-
-                    if(mqw.isAllCheckBoxUnchecked() && tfqw.isAllRadioButtonUnchecked()) {
-                        Toast toast = Toast.makeText(getContext(), "Veuillez chocher au moins une réponse", Toast.LENGTH_LONG);
-                        toast.show();
-                        return;
-                    }
-
-                    if (mqw.getVisibility() == View.VISIBLE) {
-                        userResponses.add(mqw.getUserResponses());
-                        List<CheckBox> responseCheckBox = mqw.getReponsesCheckBox();
-                        for (CheckBox c : responseCheckBox) {
-                            if (c.getText().toString().equals(question.getCorrect_answer()))
-                                c.setTextColor(Color.GREEN);
-                            else c.setTextColor(Color.RED);
-                        }
-                    } else if (tfqw.getVisibility() == View.VISIBLE) {
-                        userResponses.add(tfqw.getUserResponses());
-                        List<RadioButton> responseCheckBox = tfqw.getReponsesRadioButton();
-                        for (RadioButton c : responseCheckBox) {
-                            if (c.getText().toString().equals(question.getCorrect_answer()))
-                                c.setTextColor(Color.GREEN);
-                            else c.setTextColor(Color.RED);
-                        }
-                    }
-
-                    Handler handler = new Handler();
-                    handler.postDelayed(() -> {
-                        if (mqw.getVisibility() == View.VISIBLE)
-                            mqw.reset();
-                        if (tfqw.getVisibility() == View.VISIBLE)
-                            tfqw.reset();
-                    }, 1800);
-                    handler.postDelayed(() -> {
-                        System.out.println("----------------" + listQuestions.getResults().size());
-                        if (listQuestions.getResults().size() == 0) {
-
-                            next.setText("Terminer");
-                            next.setOnClickListener(view1 -> {
-                                if (mqw.getVisibility() == View.VISIBLE) {
-                                    userResponses.add(mqw.getUserResponses());
-                                    List<CheckBox> responseCheckBox = mqw.getReponsesCheckBox();
-                                    for (CheckBox c : responseCheckBox) {
-                                        if (c.getText().toString().equals(question.getCorrect_answer()))
-                                            c.setTextColor(Color.GREEN);
-                                        else c.setTextColor(Color.RED);
-                                    }
-                                } else if (tfqw.getVisibility() == View.VISIBLE) {
-                                    userResponses.add(tfqw.getUserResponses());
-                                    List<RadioButton> responseCheckBox = tfqw.getReponsesRadioButton();
-                                    for (RadioButton c : responseCheckBox) {
-                                        if (c.getText().toString().equals(question.getCorrect_answer()))
-                                            c.setTextColor(Color.GREEN);
-                                        else c.setTextColor(Color.RED);
-                                    }
-                                }
-                                FragmentTransaction t = getParentFragmentManager().beginTransaction();
-                                t.replace(R.id.nav_host_fragment, EndFragment.newInstance(userResponses));
-                                t.commit();
-
-
-                            });
-
-
-                        }
-                        if (listQuestions.getResults().size() != 0) {
-                            Question question1 = listQuestions.getResults().remove(0);
-                            if (listQuestions.getResults().size() == 0) {
-
-                                next.setText("Terminer");
-                                next.setOnClickListener(view1 -> {
-                                    if (mqw.getVisibility() == View.VISIBLE) {
-                                        userResponses.add(mqw.getUserResponses());
-                                        List<CheckBox> responseCheckBox = mqw.getReponsesCheckBox();
-                                        for (CheckBox c : responseCheckBox) {
-                                            if (c.getText().toString().equals(question.getCorrect_answer()))
-                                                c.setTextColor(Color.GREEN);
-                                            else c.setTextColor(Color.RED);
-                                        }
-                                    } else if (tfqw.getVisibility() == View.VISIBLE) {
-                                        userResponses.add(tfqw.getUserResponses());
-                                        List<RadioButton> responseCheckBox = tfqw.getReponsesRadioButton();
-                                        for (RadioButton c : responseCheckBox) {
-                                            if (c.getText().toString().equals(question.getCorrect_answer()))
-                                                c.setTextColor(Color.GREEN);
-                                            else c.setTextColor(Color.RED);
-                                        }
-                                    }
-                                    FragmentTransaction t = getParentFragmentManager().beginTransaction();
-                                    t.replace(R.id.nav_host_fragment, EndFragment.newInstance(userResponses));
-                                    t.commit();
-
-
-                                });
-
-
-                            }
-                            if (question1.getType().equals("multiple")) {
-                                mqw.setVisibility(View.VISIBLE);
-                                tfqw.setVisibility(View.INVISIBLE);
-                                mqw.setQuestion(question1);
-                            } else {
-                                tfqw.setVisibility(View.VISIBLE);
-                                mqw.setVisibility(View.INVISIBLE);
-                                tfqw.setQuestion(question1);
-                            }
-                            question = question1;
-                        }
-                    }, 2000);
-
-
-                }
-            });
-        }
-        return root;
     }
 
 }
